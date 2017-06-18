03bd30d7fc64af8830bb4ecd3bbc18be984d1d9a
dafty235 <dafty235@gmail.com>
2017-06-18 23:23:07 +0100

    Consistency fixes

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 'use strict';
 
 var nodeModel = require('./lib/node');
-
 var node = new nodeModel();
-
 var gracefulShutdown = function() {
 	console.log('');
-    console.error("xxx", "sys", "Received kill signal, shutting down gracefully.");
+    console.error("ARK", "sys", "Received kill signal, shutting down gracefully.");
 
     node.stop();
-    console.info("xxx", "sys", "Closed node watcher");
+    console.info("ARK", "sys", "Closed node watcher");
 
     setTimeout(function(){
         console.info("xxx", "sys", "Closed out remaining connections.");
@@ -17,16 +15,12 @@ var gracefulShutdown = function() {
     }, 1000);
 }
 
-// listen for TERM signal .e.g. kill
 process.on('SIGTERM', gracefulShutdown);
-
-// listen for INT signal e.g. Ctrl-C
 process.on('SIGINT', gracefulShutdown);
-
-// listen for shutdown signal from pm2
 process.on('message', function(msg) {
-	if (msg == 'shutdown')
+	if (msg == 'shutdown') {
 		gracefulShutdown();
+	}
 });
 
 module.exports = node;
